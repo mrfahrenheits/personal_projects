6c4dac2dd8545ebb793a18d7437528f694414715
Nicholas <nicholas.g.selistre@hotmail.com>
2026-07-02 16:50:16 +0100

    first commit

---
+++ log-automation-system/main.py
@@ -0,0 +1,11 @@
+from collector.linux_collector import LinuxLogCollector
+# from normalizer.normalizer import normalize_log
+
+def process_line(line):
+    print(f'[LOG] {line}')
+    # normalized = normalize_log(line, source="linux")
+    # print(normalized)
+
+# if __namẹ__ == "__main__":
+collector = LinuxLogCollector("/var/log/auth.log", process_line)
+collector.start()    
