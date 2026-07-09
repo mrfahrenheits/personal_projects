7fe7fc155a52c16ecefacb6747cf22ed769ce41a
Nicholas <nicholas.g.selistre@hotmail.com>
2026-07-09 17:00:17 +0100

    Alteraçao em main + finalizaçao de normalizer

--- log-automation-system/main.py
+++ log-automation-system/main.py
@@ -1,10 +1,10 @@
 from collector.linux_collector import LinuxLogCollector
-# from normalizer.normalizer import normalize_log
+from normalizer.normalizer import normalize_log
 
 def process_line(line):
-    print(f'[LOG] {line}')
-    # normalized = normalize_log(line, source="linux")
-    # print(normalized)
+    # print(f'[LOG] {line}')
+    normalized = normalize_log(line, source="linux")
+    print(normalized)
 
 # if __namẹ__ == "__main__":
 collector = LinuxLogCollector("/var/log/auth.log", process_line)
