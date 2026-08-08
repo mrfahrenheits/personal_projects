479a0de4a677efea15174b8336ddd063f550dae4
mrfahrenheits <nicholas.g.selistre@hotmail.com>
2026-08-08 11:45:41 +0100

    Correções de erros de digitação e ajustes no código

--- logSentilnel-CLI/main.py
+++ logSentilnel-CLI/main.py
@@ -1,11 +1,15 @@
 from collector.linux_collector import LinuxLogCollector
 from normalizer.normalizer import normalize_log
+from analyzer.rules_engine import RulesEngine
+
+def alert_handler(alert):
+    print(f"[ALERT] {alert}")
+
+engine = RulesEngine(alert_callback=alert_handler)
 
 def process_line(line):
-    # print(f'[LOG] {line}')
-    normalized = normalize_log(line, source="linux")
-    print(normalized)
+    event = normalize_log(line, source="linux")
+    engine.process_event(event)
 
-# if __namẹ__ == "__main__":
 collector = LinuxLogCollector("/var/log/auth.log", process_line)
 collector.start()    
